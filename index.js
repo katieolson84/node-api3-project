a2f61037f675bc4bb1217cb7e2499cef730bc040
katieolson84 <katieolson84@gmail.com>
2021-03-11 16:05:45 -0700

    added production text to knexfile

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 8000
 
+
 server.listen(port, () => {
     console.log(`\n*** Server is now running on http://localhost:${port} ***\n`);
 });
